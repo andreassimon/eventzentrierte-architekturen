fd94dfc79f675104e4793a0de54a74ae118d6d86
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-09-07 19:26:24 +0200

    Wrote failing test for publishing an event on an event bus

---
+++ src/main/java/eu/ijug/EventBus.java
@@ -0,0 +1,15 @@
+package eu.ijug;
+
+public class EventBus {
+
+	public void publish(Object event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void registerEventHandler(EventHandler eventHandler) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/eu/ijug/EventBusTest.java
@@ -0,0 +1,36 @@
+package eu.ijug;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Test;
+
+public class EventBusTest {
+	public class TestEvent {
+
+	}
+
+	EventBus eventBus = new EventBus();
+	
+	@Test
+	public void itShouldPublishAnEventToRegisteredEventHandlers() {
+		// Given 
+		TestEvent event = new TestEvent();
+		
+		final AtomicBoolean eventHandlerCalled = new AtomicBoolean(false);
+		eventBus.registerEventHandler(new EventHandler() {
+			@SuppressWarnings("unused")
+			public void on(TestEvent event) {
+				eventHandlerCalled.set(true);
+			}
+		});
+		
+		// When
+		eventBus.publish(event);
+		
+		// Expect
+		assertTrue(eventHandlerCalled.get());
+	}
+	
+}
