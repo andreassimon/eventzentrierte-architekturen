991a04147952ac7f6a39b3ecb903b8f843c1822e
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-09-07 21:40:09 +0200

    Publish event when storing it

--- src/main/java/eu/ijug/EventStore.java
+++ src/main/java/eu/ijug/EventStore.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class EventStore {
 	List<Event> events = new ArrayList<>();
-	private EventBus eventBus;
+	EventBus eventBus;
 	
 	public EventStore(EventBus eventBus) {
 		this.eventBus = eventBus;
@@ -13,6 +13,7 @@ public class EventStore {
 
 	public void storeEvent(Event event) {
 		events.add(event);
+		eventBus.publish(event);
 	}
 
 	public Iterable<Event> getAllEvents() {
