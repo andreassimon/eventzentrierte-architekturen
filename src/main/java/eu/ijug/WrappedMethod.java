5f4db6093bec06f7df67cd0bd159fc5fc8aed3f8
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-09-07 19:30:13 +0200

    Added hashCode and equals methods to WrappedMethod

--- src/main/java/eu/ijug/WrappedMethod.java
+++ src/main/java/eu/ijug/WrappedMethod.java
@@ -22,4 +22,35 @@ public class WrappedMethod {
 		}
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
+		result = prime * result + ((that == null) ? 0 : that.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WrappedMethod other = (WrappedMethod) obj;
+		if (method == null) {
+			if (other.method != null)
+				return false;
+		} else if (!method.equals(other.method))
+			return false;
+		if (that == null) {
+			if (other.that != null)
+				return false;
+		} else if (!that.equals(other.that))
+			return false;
+		return true;
+	}
+
 }
